c608b9152b98a8c6c3a22c9c60486e17083f7a61
mageecd <mageecd@masters.edu>
2019-04-10 12:41:31 -0700

    Did some testing and changed some tabs/spaces

--- ClientSocket.py
+++ ClientSocket.py
@@ -5,16 +5,16 @@ def Main():
     port = 5001
 
     mySocket = socket.socket()
-    mySocket.connect((host,port))
+    mySocket.connect((host, port))
 
     message = input(" ? ")
 
     while message != 'q':
         mySocket.send(message.encode())
-	data = mySocket.recv(1024).decode()
+        data = mySocket.recv(1024).decode()
 
-	print ('Received from server: ' + data)
-	message = input(" ? ")
+    print ('Received from server: ' + data)
+    message = input(" ? ")
 
     mySocket.close()
 
